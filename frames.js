353e2a4a0890e85495bb44affdd31b30e38555fa
SpeckyYT <Specky.fans@gmail.com>
2021-11-06 18:11:25 +0100

    updated to using sharp

--- frames.js
+++ frames.js
@@ -1,6 +1,4 @@
-const { promisify } = require('util');
-const jimp = require('jimp');
-const read = promisify(jimp.read);
+const sharp = require('sharp');
 const deepEqual = require('deep-equal');
 
 function equal(...objs){
@@ -12,11 +10,19 @@ function flattenNumber(number,colorRound){
 }
 
 function flattenColor(color,colorRound){
-    const newColor = {}
-    newColor.r = flattenNumber(color.r,colorRound)
-    newColor.g = flattenNumber(color.g,colorRound)
-    newColor.b = flattenNumber(color.b,colorRound)
-    return newColor
+    return color && {
+        r: flattenNumber(color.r,colorRound),
+        g: flattenNumber(color.g,colorRound),
+        b: flattenNumber(color.b,colorRound),
+    }
+}
+
+function getColor(image,offset){
+    return image && {
+        r: image[offset],
+        g: image[offset+1],
+        b: image[offset+2],
+    }
 }
 
 module.exports = async ({
@@ -35,35 +41,45 @@ module.exports = async ({
     OPTIMIZE_PREVIOUS_FRAME,
     OPTIMIZE_NEXT_FRAME,
 }) => {
-    const readImage = async path =>
-        path && (await read(path)).crop(CROP_X, CROP_Y,CROP_WIDTH, CROP_HEIGHT).resize(width,height)
+    const readImage = async path => {
+        return sharp(path)
+        .extract({
+            left: CROP_X,
+            top: CROP_Y,
+            width: CROP_WIDTH,
+            height: CROP_HEIGHT,
+        })
+        .resize(width,height)
+    }
 
-    const currImage = await readImage(currentPath);
-    const prevImage = await readImage(previousPath);
-    const nextImage = await readImage(nextPath);
+    const [
+        currImage,
+        prevImage,
+        nextImage,
+    ] = await Promise.all(
+        [
+            currentPath,
+            OPTIMIZE_PREVIOUS_FRAME && previousPath,
+            OPTIMIZE_NEXT_FRAME && nextPath,
+        ].map(async p => p && [...await (await readImage(p)).raw().toBuffer()])
+    );
 
     const output = []
 
     for(let x = 0; x < width; x++){
         for(let y = 0; y < height; y++){
-            function getColor(image){
-                return flattenColor(
-                    jimp.intToRGBA(
-                        image ?
-                            image.getPixelColor(x,y) : 0
-                    ), COLOR_PRECISION
-                );
-            }
-            function send(){
-                return output.push([x,y,Object.values(getColor(currImage))].join(','));
-            }
-            if(OPTIMIZE_PREVIOUS_FRAME && index > 0 && !equal(getColor(currImage),getColor(prevImage))){
-                send();
-            }else if(OPTIMIZE_NEXT_FRAME && index < length-1 && !equal(getColor(currImage),getColor(nextImage))){
-                send();
-            }else if(OPTIMIZE_PREVIOUS_FRAME && index == 0 || OPTIMIZE_NEXT_FRAME && index == length-1){
-                send();
-            }
+            const offset = x * 3 + y * width * 3
+
+            const currColor = flattenColor(getColor(currImage,offset),COLOR_PRECISION)
+            const prevColor = flattenColor(getColor(prevImage,offset),COLOR_PRECISION)
+            const nextColor = flattenColor(getColor(nextImage,offset),COLOR_PRECISION)
+
+            if(
+                (prevImage && !equal(currColor,prevColor)) ||
+                (nextImage && !equal(currColor,nextColor)) ||
+                (OPTIMIZE_PREVIOUS_FRAME && index == 0) ||
+                (OPTIMIZE_NEXT_FRAME && index + 1 == length)
+            ) output.push([x,y,Object.values(currColor)].join(','));
         }
     }
 
