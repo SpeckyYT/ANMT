3cfadb70dcf9fae5dba350771ec1925d48799ba7
SpeckyYT <Specky.fans@gmail.com>
2021-12-28 18:18:38 +0100

    added "extract frames"

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,45 @@ use std::{
     path::{
         Path, PathBuf,
     },
+    process::{
+        Command,
+        Output,
+    },
+    thread,
 };
 // use image::*;
 
-const SUPPORTED_VIDEO_FORMATS: [&str; 7] = [
-    "mp4",
+const SUPPORTED_VIDEO_FORMATS: [&str; 9] = [
+    "apng",
     "avi",
-    "mov",
-    "wm",
     "flv",
     "gif",
-    "apng",
+    "mkv",
+    "mov",
+    "mp4",
+    "mvi",
+    "wmv",
 ];
 
 fn main() {
-    let video_folder = Path::new("videos");
-    let frames_folder_buffer = video_folder.join("frames");
-    let frames_folder = frames_folder_buffer.as_path();
+    let video_folder = Path::new("videos").to_path_buf();
+    let frames_folder = video_folder.join("frames");
+    let output_folder = video_folder.join("output");
 
+    summon_folder(&video_folder);
     summon_folder(&frames_folder);
+    summon_folder(&output_folder);
 
     let video_files = find_files(&video_folder, &SUPPORTED_VIDEO_FORMATS);
 
-    println!("{:#?}", video_files);
+    manage_extract_frames(&frames_folder, &video_files);
+    manage_process_frames(&frames_folder, &video_files);
 }
 
 fn summon_folder(folder: &Path) {
-    if !folder.is_dir() {
+    if !folder.exists() {
         let backwards = folder.join("..");
-        if !backwards.is_dir() { summon_folder(&backwards) }
+        if !backwards.exists() { summon_folder(&backwards) }
         fs::create_dir(folder).unwrap();
     }
 }
@@ -55,4 +65,64 @@ fn find_files(folder: &Path, extensions: &[&str]) -> Vec<PathBuf> {
     })
     .map(|a| a.unwrap().path())
     .collect();
+}
+
+fn manage_extract_frames(frames_folder: &PathBuf, video_files: &Vec<PathBuf>){
+    let mut threads = vec![];
+    for video_file in video_files {
+        let frames_folder_clone = frames_folder.clone();
+        let video_file_clone = video_file.clone();
+        threads.push(thread::spawn(move || {
+            extract_frames(&frames_folder_clone, &video_file_clone);
+        }));
+    }
+    for current_thread in threads {
+        current_thread.join().expect("`manage_extract_frames` thread didn't end successfully.");
+    }
+}
+
+fn extract_frames(frames_folder: &PathBuf, file_path: &PathBuf) -> Output {
+    let file_name = file_path.file_stem().unwrap().to_str().unwrap();
+    let current_folder = frames_folder.join(file_name);
+
+    summon_folder(&current_folder);
+
+    return Command::new("ffmpeg")
+    .args([
+        "-i",
+        file_path.to_str().unwrap(),
+        current_folder.join(
+            format!(
+                "{}.%{}d.png",
+                file_name,
+                "6",
+            ),
+        ).to_str().unwrap(),
+    ])
+    .spawn()
+    .expect("Failed to execute FFMPEG")
+    .wait_with_output()
+    .expect("Error while running FFMPEG");
+}
+
+// copy paste moment (a macro could help?)
+fn manage_process_frames(frames_folder: &PathBuf, video_files: &Vec<PathBuf>){
+    let mut threads = vec![];
+    for video_file in video_files {
+        let frames_folder_clone = frames_folder.clone();
+        let video_file_clone = video_file.clone();
+        threads.push(thread::spawn(move || {
+            process_frames(&frames_folder_clone, &video_file_clone);
+        }));
+    }
+    for current_thread in threads {
+        current_thread.join().expect("`manage_process_frames` thread didn't end successfully.");
+    }
+}
+
+fn process_frames(frames_folder: &PathBuf, file_path: &PathBuf) {
+    let file_name = file_path.file_stem().unwrap().to_str().unwrap();
+    let current_folder = frames_folder.join(file_name);
+
+    // <insert frames preprocessing code here>
 }
