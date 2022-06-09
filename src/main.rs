f0fd23f2e13c821450ec2a9b7f63abeee791dac9
SpeckyYT <Specky.fans@gmail.com>
2022-06-09 21:59:29 +0200

    made functions for folders (not needed ig)

--- src/main.rs
+++ src/main.rs
@@ -58,25 +58,33 @@ const SUPPORTED_VIDEO_FORMATS: [&str; 9] = [
 ];
 
 fn main() {
-    let video_folder = Path::new("videos").to_path_buf();
-    let frames_folder = video_folder.join("frames");
-    let output_folder = video_folder.join("output");
+    util::summon_folder(&video_folder());
+    util::summon_folder(&frames_folder());
+    util::summon_folder(&output_folder());
 
-    util::summon_folder(&video_folder);
-    util::summon_folder(&frames_folder);
-    util::summon_folder(&output_folder);
-
-    let video_files = util::find_files(&video_folder, &SUPPORTED_VIDEO_FORMATS);
+    let video_files = util::find_files(&video_folder(), &SUPPORTED_VIDEO_FORMATS);
 
     for video_file in video_files {
         let mut video = Video::new(video_file);
 
-        video.extract_frames(&frames_folder)
+        video.extract_frames(&frames_folder())
             .wait_with_output()
             .map_err(|err| format!("Error while running FFMPEG: {}", err))
             .unwrap();
 
-        video.process_frames(&frames_folder);
-        video.output_frames(&output_folder);
+        video.process_frames(&frames_folder());
+        video.output_frames(&output_folder());
     }
+}
+
+fn video_folder() -> PathBuf {
+    return Path::new("videos").to_path_buf();
+}
+
+fn frames_folder() -> PathBuf {
+    return video_folder().join("frames");
+}
+
+fn output_folder() -> PathBuf {
+    return video_folder().join("output");
 }
